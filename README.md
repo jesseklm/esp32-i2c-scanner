7fafd3689f3c963fc81226cd63f8401ab6e2e724
jesseklm <j.kleemann@tu-bs.de>
2022-09-01 22:48:25 +0200

    first version

---
+++ README.md
@@ -0,0 +1,11 @@
+# ESP32 I2C Scanner
+
+## Inspired by the following
+- [Nick Gammon](http://www.gammon.com.au/forum/?id=10896&reply=6#reply6)
+- [Rajendra Jain](https://github.com/jainrk/i2c_port_address_scanner)
+
+## Usage
+
+1. change `pins`-array to all pins you want to test
+2. set your board in `platformio.ini` (currently `lolin_s2_mini`)
+3. build and upload
---
+++ src/main.cpp
@@ -0,0 +1,57 @@
+/*
+ ESP32 I2C Scanner
+ inspired by the following:
+ Nick Gammon http://www.gammon.com.au/forum/?id=10896&reply=6#reply6
+ Rajendra Jain https://github.com/jainrk/i2c_port_address_scanner
+*/
+
+#include <Arduino.h>
+#include <Wire.h>
+
+int pins[] = {33, 35};  // pins to test
+
+void scanAddresses() {
+  for (int address = 0x1; address <= 0x7F; address++) {
+    Wire.beginTransmission(address);
+    switch (Wire.endTransmission()) {
+      case 0:
+        Serial.print("I2C device found at address 0x");
+        if (address < 0x10) Serial.print("0");
+        Serial.print(address, HEX);
+        Serial.println(" !");
+        break;
+      case 4:
+        Serial.println("I2C in slave mode!");
+        return;
+      default:
+        continue;
+    }
+  }
+}
+
+void scanPorts() {
+  for (int i = 0; i < sizeof(pins) / sizeof(int); i++) {
+    for (int j = 0; j < sizeof(pins) / sizeof(int); j++) {
+      if (i == j) continue;
+      Serial.print("> (SDA : SCL) - (" + String(pins[i]) + " : " + String(pins[j]) + ")");
+      if (pins[i] == SDA) Serial.print(" - DEFAULT SDA");
+      if (pins[j] == SCL) Serial.print(" - DEFAULT SCL");
+      Serial.println(" <");
+      Wire.begin(pins[i], pins[j]);
+      scanAddresses();
+      Wire.end();
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) delay(10);
+  delay(1000);
+  Serial.println("Scanning...");
+  scanPorts();
+  Serial.println("done");
+}
+
+void loop() {
+}
